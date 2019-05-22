a57c3ce4de32bfcd4071b6892fd4df602f4fb3bc
khushwanth <khushwanth.chhetri@gmail.com>
2019-05-22 22:12:40 +0600

    Revert

--- index.js
+++ index.js
@@ -1,47 +1,45 @@
 var socket = require('socket.io'), http = require('http'),
 server = http.createServer(), socket = socket.listen(server);
 var mysql=require('mysql');
-var io=require('socket.io')(http);
 
-var con = mysql.createConnection({
-  host:'localhost',
-  user:'root',
-  password:'',
-  database:'ecounselling',
-});
+var con=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'ecounselling'
+})
 
-let usersocket='';
-socket.on('connection',(socket)=>{
-    console.log('connected');
-    socket.on('message', function(data) {
-        con.connect(function() {
-            con.query("UPDATE users SET socket_id = '"+socket.id+"' WHERE name = '"+data.sender+"'", function(err,result) {
-                console.log(result);
-            }); 
+socket.on('connection', function(connection) {
+    console.log('User Connected');
+    console.log(connection.id);
+    con.connect(function() {
+        con.query("UPDATE users SET socket_id = '"+connection.id+"' WHERE name = 'user'", function(result) {
+            console.log(result);
         });
+    });
 
+    connection.on('message', function(data) {
         console.log("Saving the Chat message in database...");
         console.log(data);
         con.connect(function() {
-            con.query("INSERT INTO chatinfo(sender, reciever, message) VALUES('"+data.sender+"','"+data.reciever+"','"+data.message+"')", function(err,result) {
-                console.log("inserted");
+            con.query("INSERT INTO chat(sender, receiver, message) VALUES('"+data.sender+"','"+data.reciever+"','"+data.message+"')", function(result) {
+                console.log(result);
             });
         });
+        //socket.to('uTMp6y0U4Uo1zHUnAAAB').emit('message', data.message);
+        socket.emit('message', {message: data.message, sender: data.sender, receiver: data.receiver});
+    });
 
-        con.connect(function() {
-            con.query("SELECT socket_id FROM users WHERE name='"+data.reciever+"'",function(err,result){
-                var socketid=JSON.stringify(result);
-                var sok=JSON.parse(socketid);
-                usersocket=sok[0].socket_id;
-                this.usersocket=`${usersocket}`;
-                console.log(`${usersocket}`); 
-        
-                `${usersocket}`.emit('message', {message: data.message, sender: data.sender, receiver: data.receiver});     
-            });
-        });       
+    socket.on('add-message', (message) => {
+        io.emit('message', {text: message.text, from: socket.sender, created: new Date()});
+    });
+
+    socket.on('disconnect', function(){
+        io.emit('users-changed', {user: socket.nickname, event: 'left'});
+        console.log('User Disconnected');
     });
 });
 
-server.listen(3000, function() {
+server.listen(3000, function(){
     console.log('Server started');
 });
